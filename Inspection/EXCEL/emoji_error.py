4e53ac982278277209ff7be078b392880d199976
joyeom <yeomjiho@berkeley.edu>
2024-04-12 15:48:17 +0900

    updated emoji extraction method by using emoji library

--- Inspection/EXCEL/emoji_error.py
+++ Inspection/EXCEL/emoji_error.py
@@ -2,17 +2,22 @@ import pandas as pd
 import re
 import numpy as np
 from Inspection.emoji import emojis
+import emoji
 
 # check emoji
-emoji_pattern = (
-    emojis
-    + r'|[\U0001F300-\U0001F5FF\U0001F600-\U0001F64F\U0001F680-\U0001F6FF\u2600-\u26FF\u2700-\u27BF\◦●•■]|"""|^- |^–'
-)
+emoji_pattern = r'[\U0001F300-\U0001F5FF\U0001F600-\U0001F64F\U0001F680-\U0001F6FF\u2600-\u26FF\u2700-\u27BF\◦●•■]|"""|^- |^–'
 
+bb_pattern = r'[●•■]|"""|^- |^–'
 
 def extract_emojis(text):
     if pd.notna(text):
-        return ", ".join(re.findall(emoji_pattern, str(text)))
+        extracted_emojis = []
+        for e in emoji.emoji_list(text):
+            extracted_emojis.append(e["emoji"])
+
+        extracted_emojis += re.findall(bb_pattern, str(text))
+
+        return ", ".join(extracted_emojis)
     else:
         return np.nan
 
