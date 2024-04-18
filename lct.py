92e2bacbd436d4400d41ba4f5f3ee22382c28ea7
joyeom <yeomjiho@berkeley.edu>
2024-04-18 12:04:40 +0900

    modified lct.py to tabs instead of radio

--- lct.py
+++ lct.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pandas as pd
+import st_pop_up_component as sp
+import numpy as np
+import random
 
 st.set_page_config(page_title="LCT", page_icon="./LCT/Flitto_symbol.jpg")
 st.title("LCT")
@@ -61,56 +65,91 @@ class Main(Widget):
         self.__DEFAULT_STATE["pair_dict"][excel] = json
 
     def initUI(self):
-        col1, col2 = st.columns(2)
-        with col1:
-            option = st.radio(
-                "Select environment",
-                key="environment",
-                options=["json -> excel", "excel -> json"],
+        tab1, tab2 = st.tabs(["json -> excel", "excel -> json"])
+
+        with tab1:
+            self.__DEFAULT_STATE["environment"] = "json -> excel"
+            tab1.subheader("json -> excel")
+
+            uploaded_file = st.file_uploader(
+                "Choose a File", accept_multiple_files=True, type=".json"
             )
-        with col2:
-            if option == "json -> excel":
-                st.subheader("json -> excel")
-                uploaded_file = st.file_uploader(
-                    "Choose a File", accept_multiple_files=True, type=".json"
+            self.__DEFAULT_STATE["uploaded_file"] = uploaded_file
+            need_inspect = st.toggle("내부검수 추가")
+            self.__DEFAULT_STATE["need_inspect"] = need_inspect
+            tab1_next_button = st.button("Next", key="tab1")
+            if tab1_next_button:
+                st.spinner("generating files....")
+                # call json to excel class
+                # if need_inspect add inspect
+                # else convert it and make it download in zip file
+
+        with tab2:
+            self.__DEFAULT_STATE["environment"] = "excel -> json"
+            tab2.subheader("excel -> json")
+            st.text("엑셀과 원문 json을 모두 짝지어서 올려주세요")
+            # Create an empty placeholder for dynamic content
+            placeholder = st.empty()
+            ct = len(self.__DEFAULT_STATE["pair_dict"].keys())
+
+            ej_col1, arrow, ej_col2 = st.columns([3, 2, 3])
+
+            with ej_col1:
+                excel = st.file_uploader(
+                    "Upload Excel",
+                    accept_multiple_files=True,
+                    type=".xlsx",
+                    key=f"excel_{ct}",
                 )
-                self.__DEFAULT_STATE["uploaded_file"] = uploaded_file
-                need_inspect = st.toggle("include inspection")
-                self.__DEFAULT_STATE["need_inspect"] = need_inspect
 
-                next_button = st.button("button")
-                if next_button:
-                    st.spinner("generating files....")
-                    # call json to excel class
-                    # if need_inspect add inspect
-                    # else convert it and make it download in zip file
+            with arrow:
+                st.text("➡️")
 
-            elif option == "excel -> json":
-                st.subheader("검수완료된 엑셀 값을 json에다 덮힐 것 입니다")
-                st.text("엑셀과 원문 json을 모두 짝지어서 올려주세요")
-                # Create an empty placeholder for dynamic content
-                placeholder = st.empty()
-                with placeholder.container():
-                    pair_dict_keys_count = len(self.__DEFAULT_STATE["pair_dict"].keys())
-                    self.create_pair_uploader(pair_dict_keys_count)
-                    # Function to add more file uploaders dynamically
-                    st.button(
-                        "add_more",
-                        type="primary",
-                        on_click=placeholder.write(
-                            self.create_pair_uploader(
-                                len(self.__DEFAULT_STATE["pair_dict"].keys())
-                            )
-                        ),
+            with ej_col2:
+                json = st.file_uploader(
+                    "Upload Json",
+                    accept_multiple_files=True,
+                    type=".json",
+                    key=f"json_{ct}",
+                )
+
+            if excel and json:
+
+                pair_objects = [{"Excel": e, "Json": j} for e, j in zip(excel, json)]
+
+                for i, pair_obj in enumerate(pair_objects):
+                    st.write(f"Pair {i+1}:")
+                    st.write(
+                        f"**Excel**: {pair_obj['Excel'].name}, **JSON**: {pair_obj['Json'].name}"
                     )
 
-                submitted = st.button("submit")
-                if submitted:
-                    st.spinner("generating files....")
+                tab2_next_button = st.button("Next", key="tab2")
+
+                if tab2_next_button:
+                    if len(excel) != len(json):
+                        st.error(
+                            "Excel 파일의 수와 JSON 파일의 수가 일치하지 않습니다."
+                        )
+                    else:
+                        popup_output = sp.st_custom_pop_up(
+                            message="확실히 짝이 다 맞나요?", key=random.random()
+                        )
+                        print(
+                            "popup_output",
+                            popup_output,
+                            type(popup_output),
+                            "tab2_next",
+                            tab2_next_button,
+                        )
+                        popup_output
+                        if popup_output:
+                            with st.spinner("generating files...."):
+                                for e, j in zip(excel, json):
+                                    self.__DEFAULT_STATE["pair_dict"][e] = j
+                        else:
+                            tab2_next_button = False
                     # generate excel_json using pair_dict
                     # create download button then make it download as zip file
-            else:
-                raise ValueError("need to select environment")
 
         def get_state(self):
             return self.__DEFAULT_STATE
