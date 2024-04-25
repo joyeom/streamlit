5629a1cc6c616307c632664c1762bbb3a7793711
joyeom <yeomjiho@berkeley.edu>
2024-04-25 16:36:17 +0900

    removed pywin

--- home.py
+++ home.py
@@ -1,16 +1,10 @@
 import streamlit as st
-from streamlit_extras.app_logo import add_logo
-from st_pages import show_pages_from_config, add_page_title
-from st_pages import Page, Section, add_page_title, show_pages
-# def logo():
-#     add_logo("https://brunch.co.kr/@brodipage/20", height=300)
+from st_pages import show_pages_from_config
 
 
-# Either this or add_indentation() MUST be called on each page in your
-# app to add indendation in the sidebar
-
-
-st.set_page_config(page_title="Home", page_icon="./Inspection/Flitto_symbol.jpg",layout = 'wide')
+st.set_page_config(
+    page_title="Home", page_icon="./Inspection/Flitto_symbol.jpg", layout="wide"
+)
 
 show_pages_from_config(".streamlit/pages.toml")
 
@@ -28,4 +22,4 @@ footer {visibility: hidden !important;}
 </style>
 """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-#https://github.com/blackary/st_pages => 이걸로 나중에 페이지 수정하기 
+# https://github.com/blackary/st_pages => 이걸로 나중에 페이지 수정하기
